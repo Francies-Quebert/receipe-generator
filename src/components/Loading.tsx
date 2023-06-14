4141f8cf5cd17953b995bd6d98a9a714612dc455
Francies Fernandes <quebert88@gmail.com>
2023-06-14 13:40:17 +0100

    adding lazy loading to image

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import Image from  '../assets/burger.svg'
 const Loading = () => {
     return (
         <div className='w-full flex justify-center items-center h-screen'>
-            <img src={Image} alt='Loading' className='h-52 animate-spin-slow ' />
+            <img src={Image} alt='Loading' className='h-52 animate-spin-slow ' loading="eager"/>
         </div>
     )
 }
